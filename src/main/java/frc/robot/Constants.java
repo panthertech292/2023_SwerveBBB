ede01761f29c6d580e6bf64fe5d1e22088f66f28
Derp88 <62079853+Derp88@users.noreply.github.com>
2023-10-13 15:35:08 -0400

    New Teleop Drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,14 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax.IdleMode;
+
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+import frc.lib.util.COTSFalconSwerveConstants;
+import frc.lib.util.SwerveModuleConstants;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -15,6 +22,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final double kVoltageComp = 12.0;
   //Constants for our controllers and operation of the robot
   public static final class OperatorConstants {
     public static final int kDriverControllerPort = 0;
@@ -48,15 +56,20 @@ public final class Constants {
     public static final int kIntakeMotorUpID = 40;
     public static final int kIntakeMotorDownID = 41;
   }
+
+
+
   public static final class SwerveConstants{
     //Hardware Setup
     public static final int kPigeon2ID = 9;
+    public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW- (DO NOT USE, ENABLES ROBOT-CENTRIC)
     //Front Left Module 0
     public static final class Mod0 {
       public static final int driveMotorID = 1;
       public static final int angleMotorID = 2;
       public static final int canCoderID = 11;
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(277.12);
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
     //Front Right Module 1
     public static final class Mod1 {
@@ -64,6 +77,7 @@ public final class Constants {
       public static final int angleMotorID = 5;
       public static final int canCoderID = 12;
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(223.7); //Might be drifting?
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
     //Back Left Module 2
     public static final class Mod2 {
@@ -71,6 +85,7 @@ public final class Constants {
       public static final int angleMotorID = 4;
       public static final int canCoderID = 13;
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(180.7);
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
     //Back Right Module 3
     public static final class Mod3 {
@@ -78,7 +93,80 @@ public final class Constants {
       public static final int angleMotorID = 7;
       public static final int canCoderID = 14;
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(162.4);
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
-    
+    //Swerve non-module specific constants
+    //Drivetrain constants
+    public static final COTSFalconSwerveConstants chosenModule = COTSFalconSwerveConstants.SDSMK4i(COTSFalconSwerveConstants.driveGearRatios.SDSMK4i_L2);
+
+    public static final double trackWidth = Units.inchesToMeters(24.50); 
+    public static final double wheelBase = Units.inchesToMeters(24.50); 
+    public static final double wheelCircumference = chosenModule.wheelCircumference;
+
+    //Swerve Kinematics - No need to ever change this unless you are not doing a traditional rectangular/square 4 module swerve
+     public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+        new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
+        new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
+        new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+        new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+
+    //Gear ratios
+    public static final double driveGearRatio = chosenModule.driveGearRatio;
+    public static final double angleGearRatio = chosenModule.angleGearRatio;
+
+    //Speeds - Swerve Profiling Values
+    //Meters per Second */
+    public static final double maxSpeed = 4.1; //TODO: This must be tuned to specific robot
+    public static final double maxAccel = 4.1; //TODO: This must be tuned to specific robot
+
+    //Radians per Second
+    public static final double maxAngularVelocity = 10.0; //TODO: This must be tuned to specific robot
+
+    //Inverts
+    //Angle CANEncoder Invert
+    public static final boolean canCoderInvert = chosenModule.canCoderInvert;
+    //Motor Inverts
+    public static final boolean angleMotorInvert = chosenModule.angleMotorInvert;
+    public static final boolean driveMotorInvert = chosenModule.driveMotorInvert;
+
+    //Neutral Modes
+    public static final IdleMode angleNeutralMode = IdleMode.kCoast;
+    public static final IdleMode driveNeutralMode = IdleMode.kBrake;
+
+    //Current Limits
+    //Angle
+    public static final int angleContinuousCurrentLimit = 25;
+    public static final int anglePeakCurrentLimit = 40;
+    public static final double anglePeakCurrentDuration = 0.1;
+    public static final boolean angleEnableCurrentLimit = true;
+    //Drive
+    public static final int driveContinuousCurrentLimit = 35;
+    public static final int drivePeakCurrentLimit = 60;
+    public static final double drivePeakCurrentDuration = 0.1;
+    public static final boolean driveEnableCurrentLimit = true;
+
+    //PID Values
+    //Angle
+    public static final double angleKP = chosenModule.angleKP;
+    public static final double angleKI = chosenModule.angleKI;
+    public static final double angleKD = chosenModule.angleKD;
+    public static final double angleKF = chosenModule.angleKF;
+    //Drive
+    public static final double driveKP = 0.05; //TODO: This must be tuned to specific robot
+    public static final double driveKI = 0.0;
+    public static final double driveKD = 0.0;
+    public static final double driveKF = 0.0;
+
+    //Drive Motor System Identification/Characterization
+    //TODO: Update these values https://docs.wpilib.org/en/2022/docs/software/pathplanning/system-identification/introduction.html
+    //TODO: Check if dividing by 12 is right. The template code says "Divide SYSID values by 12 to convert from volts to percent output for CTRE"
+    public static final double driveKS = (0.16861 / 12); //TODO: This must be tuned to specific robot. 
+    public static final double driveKV = (2.6686 / 12); 
+    public static final double driveKA = (0.34757 / 12);
+
+    //Motor conversion factors
+    public static final double driveConversionPositionFactor = wheelCircumference / driveGearRatio;
+    public static final double driveConversionVelocityFactor = driveConversionPositionFactor / 60.0;
+    public static final double angleConversionFactor = 360.0 / angleGearRatio;
   }
 }
--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -1,32 +1,46 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
+import frc.robot.Constants;
+import frc.robot.subsystems.SwerveSubsystem;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class TeleopSwerve extends CommandBase {
-  /** Creates a new TeleopDrive. */
-  public TeleopSwerve() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+public class TeleopSwerve extends CommandBase {    
+    private SwerveSubsystem s_Swerve;    
+    private DoubleSupplier translationSup;
+    private DoubleSupplier strafeSup;
+    private DoubleSupplier rotationSup;
+    private BooleanSupplier robotCentricSup;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+    public TeleopSwerve(SwerveSubsystem s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+        this.s_Swerve = s_Swerve;
+        addRequirements(s_Swerve);
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+        this.translationSup = translationSup;
+        this.strafeSup = strafeSup;
+        this.rotationSup = rotationSup;
+        this.robotCentricSup = robotCentricSup;
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    @Override
+    public void execute() {
+        /* Get Values, Deadband*/
+        double translationVal = translationSup.getAsDouble();
+        double strafeVal = strafeSup.getAsDouble();
+        double rotationVal = rotationSup.getAsDouble();
+
+        /* Drive */
+        s_Swerve.drive(
+            new Translation2d(translationVal, strafeVal).times(Constants.SwerveConstants.maxSpeed), 
+            rotationVal * Constants.SwerveConstants.maxAngularVelocity, 
+            !robotCentricSup.getAsBoolean(), 
+            true
+        );
+    }
 }
